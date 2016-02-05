68940573667e856f9c293de611000b58cd278088
ajlopez <webmaster@ajlopez.com>
2016-02-05 14:43:04 -0300

    getState, dispatch first tests

--- test/stores.js
+++ test/stores.js
@@ -7,3 +7,32 @@ exports['create store'] = function (test) {
     test.ok(store);
     test.equal(typeof store, 'object');
 };
+
+exports['get initial state as undefined'] = function (test) {
+    var store = sredux.createStore();
+    
+    test.strictEqual(store.getState(), undefined);
+};
+
+exports['create store with initial state'] = function (test) {
+    var store = sredux.createStore(null, 42);
+    
+    test.strictEqual(store.getState(), 42);
+};
+
+exports['create store with reducer'] = function (test) {
+    function reducer(state, action) {
+        if (state === undefined)
+            return 0;
+            
+        return state + 1;
+    }
+    
+    var store = sredux.createStore(reducer);
+    
+    test.equal(store.dispatch(null), 0);
+    test.equal(store.getState(), 0);
+    test.equal(store.dispatch(null), 1);
+    test.equal(store.getState(), 1);
+};
+
